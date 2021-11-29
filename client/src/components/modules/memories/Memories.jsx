8e6a9b70d34579d9b7b68681be2a32c52477b51d
Azan Bin Zahid <47540683+azanbinzahid@users.noreply.github.com>
2021-11-29 20:16:27 +0500

    Revert "feat: add memories via cloudinary"

--- client/src/components/modules/memories/Memories.jsx
+++ client/src/components/modules/memories/Memories.jsx
@@ -1,47 +1,8 @@
-import { Container, Typography } from '@mui/material';
-import ImageGallery from 'react-image-gallery';
-import "react-image-gallery/styles/css/image-gallery.css";
-import CloudinaryUploader from './CloudinaryUploader';
-
-import { useEffect, useState } from 'react';
-
-import { fetchMemories } from "../../../utils/firebaseUtils";
-
-const Memories = ({ partyConfig, partyId, back }) => {
-
-  const [images, setImages] = useState([
-    {
-      original: "https://res.cloudinary.com/celebirthday/image/upload/kk0fqex0zafwedjaqkte.jpg",
-    }
-  ]);
-
-    useEffect(() => {
-      fetchMemories(partyId, setImages);
-      console.log(images)
-    }, [partyId]);
-
-    return (
-      <Container>
-        <Typography variant="h3" gutterBottom component="div" align="center" style={{
-          fontWeight: "bold", 
-          color: "white"
-          }}>
-          "Memories are timeless treasures of the heart."
-        <CloudinaryUploader partyId={partyId} />
-
-        </Typography>
-        <ImageGallery items={
-            Object.values(images).map((img, index) => {
-              return (
-                {
-                  original: `https://res.cloudinary.com/celebirthday/image/upload/${img}.jpg`,
-                  thumbnail: `https://res.cloudinary.com/celebirthday/image/upload/h_100,w_100/${img}.jpg`
-                }
-              );
-            })
-          } thumbnailPosition="left" />
-      </Container>
-    )
-}
-
+const Memories = ()=>{
+  return(
+    <div className="memories-room room">
+    <h1>Memories</h1>
+    </div>
+  )
+};
 export default Memories;
